9712bc4582693ab74f7f678d7b8206d4413fd81d
pgbarletta <pbarletta@gmail.com>
2022-08-05 18:57:25 +0200

    script to collect all scoring functions

---
+++ locuaz/scoringfunctions.py
@@ -0,0 +1,8 @@
+from bach import bach
+from bluues import bluues
+from evoef2 import evoef2
+from haddock import haddock
+from pisa import pisa
+from rosetta import rosetta
+
+__all__ = ("bach", "bluues", "evoef2", "haddock", "pisa", "rosetta")

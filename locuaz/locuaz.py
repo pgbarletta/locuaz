7456ae1b31ffb2cf318283350a76f494aad29d2f
pgbarletta <pbarletta@gmail.com>
2022-08-26 08:28:42 +0200

    Heavy changes on main file. Still just experimental.

--- locuaz/locuaz.py
+++ locuaz/locuaz.py
@@ -1,59 +1,48 @@
 #!/usr/bin/env python
 
 """Main module."""
-import cli
 import sys
-import os
-from io import StringIO
 from pathlib import Path
-from fileutils import FileHandle
+import logging
+
+import cli
 import projectutils as pu
-from molecules import GROTopology, GROStructure, GROComplex, PDBStructure
-from biobb_model.model.mutate import mutate
-from gromacsutils import write_chain_selection_ndx, write_non_overlapping_ndx
-from run import run_min_nvt_npt, prepare_for_mdrun
+from protocol import initialize_new_epoch
 from scoring import score, initialize_scoring_folder
+from runutils import MDrun
+from mututils import MutatorEvoEF2
+from run import run_and_score
 
 
-def main():
+def main() -> int:
 
     config = cli.main()
     work_pjct = pu.WorkProject(config)
+    this_epoch = work_pjct.epochs[-1]
+    iter_name, this_iter = next(iter(this_epoch.items()))
 
-    # Get the baseline scores
-    # last_epoch = work_pjct.epochs[-1]
-    # this_iter_name = next(iter(last_epoch))
-    # prepare_for_mdrun(work_pjct, this_iter_name)
-    # run_min_nvt_npt(work_pjct, this_iter_name, config)
-    initialize_scoring_folder(work_pjct, next(iter(work_pjct.epochs[0].keys())), config)
-    # score(work_pjct, this_iter_name, config)
+    # mutator = MutatorEvoEF2(work_pjct.config)
+    # initialize_new_epoch(work_pjct, mutator)
 
-    print("ASBHDKKJABHDSCAJSBHDC")
+    if config["main"]["mode"] == "start":
+        logging.info(f"Running base NPT run.")
+        npt = MDrun.npt(
+            this_iter.dir_handle, work_pjct=work_pjct, out_name="npt_" + work_pjct.name
+        )
+        this_iter.complex = npt(this_iter.complex)
 
-    # mut_pdb = this_iter.complex.dir_handle.dir_path / (
-    #     "mut_" + this_iter.complex.pdb.file.path.name
-    # )
+    logging.info(f"Scoring base NPT run.")
+    nframes = initialize_scoring_folder(work_pjct, iter_name)
+    score(work_pjct, iter_name, nframes)
 
-    # prope = {"mutation_list": "B:Pro3Glu", "use_modeller": False}
-    # mutate(
-    #     input_pdb_path=str(this_iter.complex.pdb.file.path),
-    #     output_pdb_path=str(mut_pdb),
-    #     properties=prope,
-    # )
+    if config["main"]["mode"] == "score":
+        return 0
 
-    # resid = 3
-    # binder_ndx = write_chain_selection_ndx(
-    #     "binder",
-    #     ["B"],
-    #     PDBStructure.from_path(mut_pdb),
-    #     this_iter.dir_handle.dir_path,
-    # )
-
-    # ndx_handle, wat_cnt = write_non_overlapping_ndx(mut_pdb, binder_ndx, resid)
-
-    # # gmx trjconv -f mut_1ppg.pdb -s mut_1ppg.pdb -o nonwat_mut_1ppg.pdb -n non_overlapping.ndx
-
-    # print(wat_cnt)
+    logging.info(f"Starting protocol.")
+    mutator = MutatorEvoEF2(work_pjct.config)
+    initialize_new_epoch(work_pjct, mutator)
+    run_and_score(work_pjct)
+    return 0
 
 
 if __name__ == "__main__":

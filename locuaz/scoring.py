5d792d312702686902526c11359a697bdaabebe0
pgbarletta <pbarletta@gmail.com>
2022-08-29 14:20:39 +0200

    cosmetic fixes and now biobb shortcut functions are not used. Instead, the class for each command is instantiated and passed to launch_biobb() for launching and error reporting.

--- locuaz/scoring.py
+++ locuaz/scoring.py
@@ -1,54 +1,57 @@
 import time
 from typing import Dict
+import logging
 from utils_scoring import extract_pdbs
 from projectutils import WorkProject
 from fileutils import DirHandle
-from biobb_analysis.gromacs.gmx_trjconv_str_ens import gmx_trjconv_str_ens
+from primitives import launch_biobb
+from biobb_analysis.gromacs.gmx_trjconv_str_ens import GMXTrjConvStrEns
 from scoringfunctions import *
 
 
-def initialize_scoring_folder(work_pjct: WorkProject, iter_name: str, config: Dict):
-    print("--- Creating scoring folder and extracting PDBs from NPT trajectory ---")
-
+def initialize_scoring_folder(work_pjct: WorkProject, iter_name: str):
     this_iter = work_pjct.epochs[-1][iter_name]
     current_dir = this_iter.dir_handle.dir_path
     this_iter.score_dir = DirHandle(current_dir / "scoring", make=True, replace=True)
+    gmx_bin: str = work_pjct.config["md"]["gmx_bin"]
 
-    # Extract target PDBs
-    ens_of_pdbs = this_iter.score_dir.dir_path / (this_iter.complex.name + ".zip")
-    props = {"selection": "target"}
-    gmx_trjconv_str_ens(
-        input_traj_path=str(this_iter.complex.tra.file.path),
-        input_top_path=str(this_iter.complex.tpr.file.path),
-        input_index_path=str(this_iter.complex.complex_ndx.path),
-        output_str_ens_path=str(ens_of_pdbs),
-        properties=props,
+    # Zip filename with the extracted PDBs
+    ens_of_pdbs = this_iter.score_dir.dir_path / (
+        "ensemble_" + this_iter.complex.name + ".zip"
     )
-    nframes_target = extract_pdbs(ens_of_pdbs, "target")
-
-    # Extract binder PDBs
-    ens_of_pdbs = this_iter.score_dir.dir_path / (this_iter.complex.name + ".zip")
-    props = {"selection": "binder"}
-    gmx_trjconv_str_ens(
-        input_traj_path=str(this_iter.complex.tra.file.path),
-        input_top_path=str(this_iter.complex.tpr.file.path),
-        input_index_path=str(this_iter.complex.complex_ndx.path),
-        output_str_ens_path=str(ens_of_pdbs),
-        properties=props,
-    )
-    nframes_binder = extract_pdbs(ens_of_pdbs, "binder")
 
     # Extract complex PDBs
-    ens_of_pdbs = this_iter.score_dir.dir_path / (this_iter.complex.name + ".zip")
-    props = {"selection": "Protein"}
-    gmx_trjconv_str_ens(
+    # TODO: this only works when there're no lipids or glyco-stuff
+    get_complex = GMXTrjConvStrEns(
         input_traj_path=str(this_iter.complex.tra.file.path),
         input_top_path=str(this_iter.complex.tpr.file.path),
-        # input_index_path=str(this_iter.complex.complex_ndx.path),
         output_str_ens_path=str(ens_of_pdbs),
-        properties=props,
+        properties={"gmx_path": gmx_bin, "selection": "Protein"},
     )
-    nframes_complex = extract_pdbs(ens_of_pdbs, "complex")
+    launch_biobb(get_complex)
+    nframes_target = extract_pdbs(ens_of_pdbs, "complex")
+
+    # Extract target PDBs
+    get_target = GMXTrjConvStrEns(
+        input_traj_path=str(this_iter.complex.tra.file.path),
+        input_top_path=str(this_iter.complex.tpr.file.path),
+        input_index_path=str(this_iter.complex.ndx.path),
+        output_str_ens_path=str(ens_of_pdbs),
+        properties={"gmx_path": gmx_bin, "selection": "target"},
+    )
+    launch_biobb(get_target)
+    nframes_complex = extract_pdbs(ens_of_pdbs, "target")
+
+    # Extract binder PDBs
+    get_binder = GMXTrjConvStrEns(
+        input_traj_path=str(this_iter.complex.tra.file.path),
+        input_top_path=str(this_iter.complex.tpr.file.path),
+        input_index_path=str(this_iter.complex.ndx.path),
+        output_str_ens_path=str(ens_of_pdbs),
+        properties={"gmx_path": gmx_bin, "selection": "binder"},
+    )
+    launch_biobb(get_binder)
+    nframes_binder = extract_pdbs(ens_of_pdbs, "binder")
 
     assert (
         nframes_target == nframes_binder == nframes_complex
@@ -57,7 +60,7 @@ def initialize_scoring_folder(work_pjct: WorkProject, iter_name: str, config: Di
     return nframes_complex
 
 
-def score(work_pjct: WorkProject, iter_name: str, config: Dict, nframes: int) -> None:
+def score(work_pjct: WorkProject, iter_name: str, nframes: int) -> None:
     start = time.time()
     this_iter = work_pjct.epochs[-1][iter_name]
 
@@ -65,14 +68,14 @@ def score(work_pjct: WorkProject, iter_name: str, config: Dict, nframes: int) ->
         scores = scorer(nframes=nframes, frames_path=this_iter.score_dir)
         if sf_name == "bluues_scorer":
             promedio = this_iter.set_score("bluues", scores[0])
-            print(f"bluues: {promedio}")
+            logging.info(f"{sf_name} average score: {promedio}")
             promedio = this_iter.set_score("bmf", scores[1])
-            print(f"bmf: {promedio}")
+            logging.info(f"bmf average score: {promedio}")
         else:
             promedio = this_iter.set_score(sf_name, scores)
-            print(promedio)
-        print(f"-------")
+            logging.info(f"{sf_name} average score: {promedio}")
 
     this_iter.write_down_scores()
-    end = time.time()
-    print(end - start)
+    logging.info(
+        f"Time elapsed during {iter_name}'s {nframes} frames scoring: {time.time() - start}"
+    )

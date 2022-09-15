094bf04fb5e9ab5922d4344e300f6d7d523b7e8c
pgbarletta <pbarletta@gmail.com>
2022-09-15 08:43:42 +0200

    New prunner functions.

---
+++ locuaz/prunner.py
@@ -0,0 +1,79 @@
+from ast import Call
+from typing import Tuple, Dict, Callable
+from queue import PriorityQueue
+
+from projectutils import Epoch, Iteration, WorkProject
+
+
+def beats_old_iter(
+    old_iter: Iteration, new_iter: Iteration, threshold: int
+) -> Tuple[bool, int]:
+
+    # Allow the user to change scoring functions mid-run and only use
+    # the subset present in both iterations.
+    scoring_functions = set(old_iter.scores.keys()) & set(new_iter.scores.keys())
+    count = sum(
+        [
+            old_iter.mean_scores[SF] > new_iter.mean_scores[SF]
+            for SF in scoring_functions
+        ]
+    )
+
+    return count > threshold, count
+
+
+def choose_top_iters(
+    prev_epoch: Epoch, this_epoch: Epoch, threshold: int
+) -> PriorityQueue:
+    better_iters: PriorityQueue = PriorityQueue()
+
+    for prev_iter in prev_epoch.top_iterations.values():
+        for iter in this_epoch.values():
+            better, rank = beats_old_iter(prev_iter, iter, threshold)
+            if better:
+                better_iters.put((-rank, iter))
+
+    return better_iters
+
+
+def top_prunner(better_iters: PriorityQueue, prune: int) -> Dict[str, Iteration]:
+    """top_prunner select, at least, the first `prun` iterations above the threshold
+
+    Args:
+        better_iters (PriorityQueue[Tuple[int, Iteration]]): iterations above the threshold,
+            sorted by approved SFs
+        prune (int): minimum number of iterations returned
+
+    Returns:
+        Dict[str, Iteration]: map of top `iter_name:Iteration` for the next epoch.
+    """
+
+    top_iterations: Dict[str, Iteration] = {}
+    for i in range(prune):
+        iter = better_iters.get()[1]
+        top_iterations[iter.iter_name] = iter
+    return top_iterations
+
+
+def adaptive_prunner(better_iters: PriorityQueue, prune: int) -> Dict[str, Iteration]:
+    """adaptive_prunner select all iterations that have the same number of approved SFs
+
+    Args:
+        better_iters (PriorityQueue[Tuple[int, Iteration]]): iterations above the threshold,
+            sorted by approved SFs
+        prune (int): unused
+
+    Returns:
+        Dict[str, Iteration]: map of top `iter_name:Iteration` for the next epoch.
+    """
+    top_iterations: Dict[str, Iteration] = {}
+    prev_count = 1
+    while not better_iters.empty():
+        # Remember, `count`, the priority, is negative.
+        count, iter = better_iters.get()
+        if count > prev_count:
+            assert len(top_iterations) > 0, f"Logical error. This can't happen."
+            break
+        prev_count = count
+        top_iterations[iter.iter_name] = iter
+    return top_iterations
---
+++ locuaz/prunners.py
@@ -0,0 +1,35 @@
+from typing import Dict, Callable
+
+from projectutils import Epoch, Iteration, WorkProject
+from prunner import choose_top_iters, adaptive_prunner, top_prunner
+
+
+prunners: Dict[str, Callable] = {
+    "adaptive": adaptive_prunner,
+    "top": top_prunner,
+}
+
+
+def prune(work_pjct: WorkProject) -> None:
+    this_epoch = work_pjct.epochs[-1]
+    try:
+        prev_epoch = work_pjct.epochs[-2]
+    except IndexError as e:
+        # Initial epoch.
+        this_epoch.top_iterations = this_epoch.iterations
+        return
+
+    threshold = work_pjct.config["scoring"]["consensus_threshold"]
+
+    better_iters_queue = choose_top_iters(prev_epoch, this_epoch, threshold)
+    if better_iters_queue.empty():
+        # All new iterations are worse than the old ones or they all failed during MD.
+        top_iterations = prev_epoch.top_iterations
+    else:
+        prunner_func = prunners[work_pjct.config["protocol"]["prunner"]]
+        prune = work_pjct.config["protocol"]["prune"]
+        # Use the required prunner
+        top_iterations = prunner_func(better_iters_queue, prune)
+
+    this_epoch.top_iterations = top_iterations
+    return

f73ca8a55c844ce38b7b6d4775a448fb48dae579
pgbarletta <pbarletta@gmail.com>
2022-09-21 15:21:58 +0200

    removed the now() utility function used for logging. Now the log writes time information on its own.

--- locuaz/primitives.py
+++ locuaz/primitives.py
@@ -13,7 +13,3 @@ def launch_biobb(biobb_obj, can_write_console_log: bool = False) -> None:
     assert (
         err == 0
     ), f"{biobb_obj} failed. Check {biobb_obj.out_log} and {biobb_obj.err_log}."
-
-
-def now() -> str:
-    return datetime.now().strftime("%H:%M:%S of the %Y-%m-%d ")

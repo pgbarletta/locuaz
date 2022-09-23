4d3a334e0897210f2ee6942fb5b2da34598ebfdf
pgbarletta <pbarletta@gmail.com>
2022-09-23 11:43:52 +0200

    split run_epoch() in run_min_nvt_epoch() and run_npt_epoch()

--- locuaz/run.py
+++ locuaz/run.py
@@ -7,7 +7,10 @@ import concurrent.futures as cf
 
 from projectutils import Epoch, Iteration, WorkProject
 from runutils import MDrun
-from scoring import score, initialize_scoring_folder
+
+def run_epoch(work_pjct: WorkProject) -> None:
+    run_min_nvt_epoch(work_pjct)
+    run_npt_epoch(work_pjct)
 
 
 def worker(task_queue: Queue, results_queue: SimpleQueue):
@@ -23,8 +26,8 @@ def worker(task_queue: Queue, results_queue: SimpleQueue):
         results_queue.put(complex)
         task_queue.task_done()
 
-
-def run_epoch(work_pjct: WorkProject) -> None:
+def run_min_nvt_epoch(work_pjct: WorkProject) -> None:
+    log = logging.getLogger(f"{work_pjct.name}")
     epoch = work_pjct.epochs[-1]
     ngpus = work_pjct.config["md"]["ngpus"]
     pinoffsets = work_pjct.config["md"]["pinoffsets"]
@@ -32,7 +35,6 @@ def run_epoch(work_pjct: WorkProject) -> None:
     with ProcessPoolExecutor(max_workers=ngpus) as ex:
         futuros_min = []
         futuros_nvt = []
-        futuros_npt = []
         gpu_id = {}
         pinoffset = {}
         for idx, (iter_name, iter) in enumerate(epoch.items()):
@@ -40,7 +42,7 @@ def run_epoch(work_pjct: WorkProject) -> None:
             gpu_id[iter_name] = idx % ngpus
             pinoffset[iter_name] = pinoffsets[idx % ngpus]
             
-            logging.info(f"Queuing MIN of the iteration {iter_name} to GPU {gpu_id[iter_name]} "
+            log.info(f"Queuing MIN of the iteration {iter_name} to GPU {gpu_id[iter_name]} "
             f"and pinoffset: {pinoffset[iter_name]}.")
 
             min = MDrun.min(iter.dir_handle, work_pjct=work_pjct, gpu_id = gpu_id[iter_name],
@@ -49,13 +51,13 @@ def run_epoch(work_pjct: WorkProject) -> None:
     
         for futu_min in cf.as_completed(futuros_min):
             if futu_min.exception():
-                logging.error(f"Exception while running MIN: {futu_min.exception()}")
+                log.error(f"Exception while running MIN: {futu_min.exception()}")
                 
             min_complex = futu_min.result()
             iter_name = '-'.join(min_complex.dir.dir_path.name.split('-')[1:])
             iter = epoch[iter_name]
             
-            logging.info(f"Queuing NVT of the iteration {iter_name} to GPU {gpu_id[iter_name]} "
+            log.info(f"Queuing NVT of the iteration {iter_name} to GPU {gpu_id[iter_name]} "
             f"and pinoffset: {pinoffset[iter_name]}.")
 
             nvt = MDrun.nvt(
@@ -65,46 +67,41 @@ def run_epoch(work_pjct: WorkProject) -> None:
 
         for futu_nvt in cf.as_completed(futuros_nvt):
             if futu_nvt.exception():
-                logging.error(f"Exception while running NVT:  {futu_nvt.exception()} ")
+                log.error(f"Exception while running NVT:  {futu_nvt.exception()} ")
                 
             nvt_complex = futu_nvt.result()
             iter_name = '-'.join(nvt_complex.dir.dir_path.name.split('-')[1:])
-            iter = epoch[iter_name]
-            
-            logging.info(f"Queuing NPT of the iteration {iter_name} to GPU {gpu_id[iter_name]} "
-            f"and pinoffset: {pinoffset[iter_name]}.")
-            npt = MDrun.npt(
-                iter.dir_handle, work_pjct=work_pjct, gpu_id = gpu_id[iter_name],
-                pinoffset=pinoffset[iter_name], out_name="npt_" + work_pjct.name)
-            futuros_npt.append(ex.submit(npt, nvt_complex))
+            epoch[iter_name].complex = nvt_complex
 
-        for futu_npt in cf.as_completed(futuros_npt):
-            if futu_npt.exception():
-                logging.error(f"Exception while running NPT:  {futu_npt.exception()} ")
-                
-            npt_complex = futu_npt.result()
-            iter_name = '-'.join(npt_complex.dir.dir_path.name.split('-')[1:])
-            iter = epoch[iter_name]
-            iter.complex = npt_complex
 
-def finish_npt_epoch(work_pjct: WorkProject) -> None:
+def run_npt_epoch(work_pjct: WorkProject) -> None:
+    log = logging.getLogger(work_pjct.name)
     epoch = work_pjct.epochs[-1]
     ngpus = work_pjct.config["md"]["ngpus"]
     prefix = work_pjct.config["main"]["prefix"]
+    pinoffsets = work_pjct.config["md"]["pinoffsets"]
 
     with ProcessPoolExecutor(max_workers=ngpus) as ex:
+        gpu_id = {}
+        pinoffset = {}
         futuros_npt = []
         for idx, (iter_name, iter) in enumerate(epoch.items()):
+            if iter.npt_started:
+                continue
             gpu_nbr = idx % ngpus
-            logging.info(f"Queuing NPT of the iteration {iter_name} to GPU {gpu_nbr}.")
+            gpu_id[iter_name] = idx % ngpus
+            pinoffset[iter_name] = pinoffsets[idx % ngpus]
+            log.info(f"Queuing NPT of the iteration {iter_name} to GPU {gpu_nbr}. "
+            f"and pinoffset: {pinoffset[iter_name]}.")
             
             npt = MDrun.npt(
-                iter.dir_handle, work_pjct=work_pjct, gpu_id = gpu_nbr, out_name=prefix + work_pjct.name)
+                iter.dir_handle, work_pjct=work_pjct, gpu_id = gpu_nbr,
+                pinoffset=pinoffset[iter_name], out_name=prefix + work_pjct.name)
             futuros_npt.append(ex.submit(npt, iter.complex))
 
         for futu_npt in cf.as_completed(futuros_npt):
             if futu_npt.exception():
-                logging.error(f"Exception while running NPT:  {futu_npt.exception()} ")
+                log.error(f"Exception while running NPT:  {futu_npt.exception()} ")
                 
             npt_complex = futu_npt.result()
             iter_name = '-'.join(npt_complex.dir.dir_path.name.split('-')[1:])

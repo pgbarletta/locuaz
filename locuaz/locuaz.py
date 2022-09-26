e56425b8ee487bfa20ae05929b5a4ea2a0118b56
pgbarletta <pbarletta@gmail.com>
2022-09-26 08:57:56 +0200

    Main file. This version should be more or less stable from now on.

--- locuaz/locuaz.py
+++ locuaz/locuaz.py
@@ -2,46 +2,63 @@
 
 """Main module."""
 import sys
-from pathlib import Path
 import logging
 
 import cli
 import projectutils as pu
 from protocol import initialize_new_epoch
-from scoring import score, initialize_scoring_folder
-from runutils import MDrun
-from mututils import MutatorEvoEF2
-from run import run_and_score
+from scoring import score
+from run import run_epoch, run_min_nvt_epoch, run_npt_epoch
+from prunners import prune
 
 
 def main() -> int:
 
     config = cli.main()
     work_pjct = pu.WorkProject(config)
-    this_epoch = work_pjct.epochs[-1]
-    iter_name, this_iter = next(iter(this_epoch.items()))
+    log = logging.getLogger(work_pjct.name)
+    log.info(f"Launching.")
 
-    # mutator = MutatorEvoEF2(work_pjct.config)
-    # initialize_new_epoch(work_pjct, mutator)
-
-    if config["main"]["mode"] == "start":
-        logging.info(f"Running base NPT run.")
-        npt = MDrun.npt(
-            this_iter.dir_handle, work_pjct=work_pjct, out_name="npt_" + work_pjct.name
-        )
-        this_iter.complex = npt(this_iter.complex)
-
-    logging.info(f"Scoring base NPT run.")
-    nframes = initialize_scoring_folder(work_pjct, iter_name)
-    score(work_pjct, iter_name, nframes)
-
-    if config["main"]["mode"] == "score":
+    if config["main"]["mode"] == "run":
+        run_epoch(work_pjct)
+        return 0
+    elif config["main"]["mode"] == "run_npt":
+        run_npt_epoch(work_pjct)
+        return 0
+    elif config["main"]["mode"] == "score":
+        for iter_name, iter in work_pjct.epochs[-1].items():
+            log.info(f"Scoring NPT run from iteration: {iter_name}")
+            score(work_pjct, iter)
         return 0
 
-    logging.info(f"Starting protocol.")
-    mutator = MutatorEvoEF2(work_pjct.config)
-    initialize_new_epoch(work_pjct, mutator)
-    run_and_score(work_pjct)
+    if not work_pjct.epochs[-1].npt_started:
+        run_min_nvt_epoch(work_pjct)
+        work_pjct.epochs[-1].npt_started = True
+
+    if not work_pjct.epochs[-1].npt_finished:
+        run_npt_epoch(work_pjct)
+        work_pjct.epochs[-1].npt_finished = True
+
+    for cnt in range(config["protocol"]["epochs"]):
+        epoch_id = work_pjct.epochs[-1].id
+
+        log.info(f"Scoring epoch {epoch_id} ({cnt} on this run).")
+        for iter_name, iter in work_pjct.epochs[-1].items():
+            log.info(f"Scoring NPT run from iteration: {iter_name}")
+            score(work_pjct, iter)
+
+        log.info(f"Prunning epoch {epoch_id}.")
+        prune(work_pjct)
+        log.info(f"Top iterations: {work_pjct.epochs[-1].top_iterations.keys()}.")
+
+        log.info(f"Initializing new epoch {epoch_id+1} ({cnt+1} on this run).")
+        initialize_new_epoch(work_pjct)
+
+        log.info(f"Running epoch {epoch_id} ({cnt} on this run).")
+        run_epoch(work_pjct)
+
+    log.info(f"Done with protocol.")
+
     return 0
 
 

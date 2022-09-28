bb9560932bb84eeabeb1388cc1d0d76a25cb14da
pgbarletta <pbarletta@gmail.com>
2022-09-28 08:50:23 +0200

    removed unused imports

--- locuaz/primitives.py
+++ locuaz/primitives.py
@@ -1,10 +1,7 @@
-from datetime import datetime
-
-
-from biobb_md.gromacs.gmxselect import Gmxselect
-from biobb_analysis.gromacs.gmx_trjconv_str import GMXTrjConvStr
-from biobb_md.gromacs.solvate import Solvate
-from biobb_md.gromacs.grompp import Grompp
+# from biobb_md.gromacs.gmxselect import Gmxselect
+# from biobb_analysis.gromacs.gmx_trjconv_str import GMXTrjConvStr
+# from biobb_md.gromacs.solvate import Solvate
+# from biobb_md.gromacs.grompp import Grompp
 
 
 def launch_biobb(biobb_obj, can_write_console_log: bool = False) -> None:

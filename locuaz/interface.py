4f6daf5b5b2c0262546315c9f87e0acf9343eb5d
Patricio <pbarletta@gmail.com>
2023-02-24 09:45:26 +0100

    get_interfacing_residues() fix resSeq numbering
    In case tleap is being used, get_interfacing_residues() will get the
    input PDB and write out a temporary PDB without water nor ions and
    renumbering the resSeq to a continuous numbering scheme.

--- locuaz/interface.py
+++ locuaz/interface.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Union, List, Set
-import warnings
+import py
 
 import freesasa
 import MDAnalysis as mda
@@ -10,13 +10,17 @@ from molecules import PDBStructure
 from fileutils import FileHandle
 
 
-def get_interfacing_residues(pdb_input: Union[PDBStructure, FileHandle, Path], chainIDs: List[str]) -> Set[int]:
+def get_interfacing_residues(pdb_input: Union[PDBStructure, FileHandle, Path],
+                             chainIDs: List[str], use_tleap: bool = False) -> Set[int]:
     """
     get_interfacing_residues(): use freesasa to get the resSeq of the binder residues that are in contact with
     the target. These can be used to guide the choice of the next mutated position.
     Args:
-        pdb_input: input PDB.
-        chainIDs: only residues belonging to these chains will be reported.
+        pdb_input (Union[PDBStructure, FileHandle, Path]): input PDB.
+        chainIDs (List[str]): only residues belonging to these chains will be reported.
+        use_tleap (bool): if True, it will renumber the resSeqs of the input PDB (on another temporary PDB)
+        as per Amber numbering scheme, that is, continuous resSeq numbers as opposed to GROMACS numbering
+        which starts at 1 on each chain.
 
     Returns:
         Set[int]: set of resSeqs from the binder that lie on the interface
@@ -25,12 +29,18 @@ def get_interfacing_residues(pdb_input: Union[PDBStructure, FileHandle, Path], c
     pdb_path = Path(pdb_input)
     u = mda.Universe(str(pdb_path))
     temp_pdb = Path(pdb_path.parent, "temp.pdb")
-    u.select_atoms("not resname SOL and not resname WAT").write(str(temp_pdb))
+    protein = u.select_atoms("not (resname SOL or resname WAT or resname CL or resname NA or resname Cl or resname Na)")
+    # Renumber resSeq when using Amber numbering since GROMACS will have renumbered the PDB
+    # to start at 1 on each chain
+    if use_tleap:
+        protein.residues.resids = range(1, len(protein.residues.resnums) + 1)
+    protein.write(str(temp_pdb))
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        structs = freesasa.structureArray(str(temp_pdb),
-                                      {"separate-chains": False, "chain-groups": ''.join(chainIDs)})
+    # Silence warnings from freesasa
+    capture_warnings = py.io.StdCaptureFD(out=True, in_=False)
+    structs = freesasa.structureArray(str(temp_pdb),
+                                          {"separate-chains": False, "chain-groups": ''.join(chainIDs)})
+    capture_warnings.reset()
 
     sasa_whole = freesasa.calc(structs[0])
     sasa_binder = freesasa.calc(structs[1])
@@ -49,16 +59,19 @@ def get_interfacing_residues(pdb_input: Union[PDBStructure, FileHandle, Path], c
 
     return interfacing_resis
 
+
 def get_freesasa_residues(pdb_input: Union[PDBStructure, FileHandle, Path], chainIDs: List[str]) -> Set[int]:
     pdb_path = Path(pdb_input)
     u = mda.Universe(str(pdb_path))
     temp_pdb = Path(pdb_path.parent, "temp.pdb")
     u.select_atoms("not resname SOL and not resname WAT").write(str(temp_pdb))
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        structs = freesasa.structureArray(str(temp_pdb),
+    # Silence warnings from freesasa
+    capture_warnings = py.io.StdCaptureFD(out=True, in_=False)
+    structs = freesasa.structureArray(str(temp_pdb),
                                       {"separate-chains": False, "chain-groups": ''.join(set(chainIDs))})
+    capture_warnings.reset()
+
     freesasa_resis = {(int(structs[1].residueNumber(i)), seq1(structs[1].residueName(i))) for i in
                       range(structs[1].nAtoms())}
     temp_pdb.unlink()

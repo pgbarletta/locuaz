f0e0a6d0979536277370aaaeb96d49552fe2ae91
Patricio <pbarletta@gmail.com>
2023-05-04 12:53:57 +0200

    Minor fixes and update to latest options

--- locuaz/pruners.py
+++ locuaz/pruners.py
@@ -16,18 +16,18 @@ def prune(work_pjct: WorkProject) -> None:
     this_epoch = work_pjct.epochs[-1]
     try:
         prev_epoch = work_pjct.epochs[-2]
-    except IndexError as e:
+    except IndexError:
         # Initial epoch.
         this_epoch.top_iterations = this_epoch.iterations
         return
 
     log = logging.getLogger(work_pjct.name)
-    threshold = work_pjct.config["scoring"]["consensus_threshold"]
+    threshold = work_pjct.config["pruning"]["consensus_threshold"]
 
     better_iters_queue = choose_top_iters(prev_epoch, this_epoch, threshold, log)
     failed_pos: Set[int] = set()
     if better_iters_queue.empty():
-        # All new iterations are worse than the old ones or they all failed during MD.
+        # All new iterations are worse than the old ones, or they all failed during MD.
         log.info(
             f"Failed epoch after mutating resSeqs: {this_epoch.mutated_positions}. "
             f"Backing up epoch {this_epoch.id}."
@@ -40,17 +40,16 @@ def prune(work_pjct: WorkProject) -> None:
             f"Successful epoch after mutating resSeqs: {this_epoch.mutated_positions} ."
         )
         pruner_func = pruners[work_pjct.config["protocol"]["pruner"]]
-        prune = work_pjct.config["protocol"].get("prune")
         # Prune as many branches as requested, using the required pruner.
-        this_epoch.top_iterations = pruner_func(better_iters_queue, prune)
+        this_epoch.top_iterations = pruner_func(better_iters_queue, work_pjct.config["protocol"].get("prune"))
 
     if work_pjct.has_failed_memory:
         work_pjct.failed_mutated_positions.appendleft(failed_pos)
 
     top_itrs_str = " ; ".join(
         [
-            f"{iter.epoch_id}-{iter.iter_name}"
-            for iter in work_pjct.epochs[-1].top_iterations.values()
+            f"{iteration.epoch_id}-{iteration.iter_name}"
+            for iteration in work_pjct.epochs[-1].top_iterations.values()
         ]
     )
     log.info(f"Top iterations: {top_itrs_str}")

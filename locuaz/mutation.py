efe819b547635f3fccd4537000a7801d5ef5cc17
Patricio <pbarletta@gmail.com>
2023-05-08 13:36:18 +0200

    Renamed new_name_resname to generate_name_resname

--- locuaz/mutation.py
+++ locuaz/mutation.py
@@ -1,7 +1,4 @@
 from attrs import define, field
-from typing import List, Tuple
-
-from locuaz.projectutils import Iteration
 
 
 @define(frozen=True)
@@ -12,28 +9,6 @@ class Mutation:
     new_aa: str = field(converter=str, kw_only=True)
     chainID_idx: int = field(converter=int, kw_only=True)
     resSeq_idx: int = field(converter=int, kw_only=True)
-
-    # This is a horrible way to do this, I'll do it properly later.
-    def new_name_resname(self, iteration: Iteration) -> Tuple[str, List[str]]:
-        iter_name = ""
-        new_iteration_resnames = []
-        for idx, (chainID, resname) in enumerate(zip(iteration.chainIDs, iteration.resnames)):
-            if idx == self.chainID_idx:
-                # This is the mutated chainID
-                new_resname = (
-                        resname[: self.resSeq_idx]
-                        + self.new_aa
-                        + resname[self.resSeq_idx + 1:]
-                )
-
-            else:
-                # This one remains the same
-                new_resname = "".join([residue for residue in resname])
-            iter_name += f"-{chainID}_{new_resname}"
-            new_iteration_resnames.append(new_resname)
-
-        # Drop the leading '-'
-        return iter_name[1:], new_iteration_resnames
 
     def get_mda_sel(self, backbone_only=True) -> str:
         return f"segid {self.chainID} and resnum {self.resSeq}" + (" and backbone" if backbone_only else "")

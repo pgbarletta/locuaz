41eaa8a7d71fd62b4423fa09f053f62818f8b133
pgbarletta <pbarletta@gmail.com>
2023-10-17 20:02:53 -0300

    Add Infinite pool DS

---
+++ locuaz/pools.py
@@ -0,0 +1,73 @@
+from typing import Iterator, TypeVar, Generic, Set, Iterable
+from random import choice
+
+T = TypeVar("T")
+
+
+class InfinitePool(Generic[T]):
+    pool: Set[T]
+    reservoir: Set[T]
+
+    def __init__(self, input_set: Iterable[T]) -> None:
+        self.reservoir = set(input_set)
+        self.pool = set(self.reservoir)
+
+    def __reset__(self) -> None:
+        if len(self.pool) == 0:
+            self.pool = set(self.reservoir)
+
+    def __contains__(self, item) -> bool:
+        return item in self.pool
+
+    def __iter__(self) -> Iterator:
+        return iter(self.pool)
+
+    def __len__(self) -> T:
+        return len(self.pool)
+
+    def __sub__(self, other: "InfinitePool") -> "InfinitePool":
+        self.__reset__()
+        return InfinitePool(self.pool - other.pool)
+
+    def __isub__(self, other: "InfinitePool") -> "InfinitePool":
+        self.__reset__()
+        self.pool -= other.pool
+        return self
+
+    def __or__(self, other: "InfinitePool") -> "InfinitePool":
+        self.__reset__()
+        return InfinitePool(self.pool | other.pool)
+
+    def __and__(self, other: "InfinitePool") -> "InfinitePool":
+        self.__reset__()
+        return InfinitePool(self.pool & other.pool)
+
+    def __repr__(self) -> str:
+        return self.pool.__repr__()
+
+    def __str__(self) -> str:
+        return self.pool.__str__()
+
+    def discard(self, item) -> None:
+        self.__reset__()
+        return self.pool.discard(item)
+
+    def pick(self) -> T:
+        self.__reset__()
+        return choice(tuple(self.pool))
+
+    def pop(self) -> T:
+        self.__reset__()
+        item = self.pick()
+        self.discard(item)
+        return item
+
+
+class BinPool(InfinitePool):
+
+    def __init__(self, input_set: Iterable[int]) -> None:
+        super().__init__(input_set)
+
+    @classmethod
+    def from_size(cls, size: int) -> "BinPool":
+        return cls(set(range(0, size)))

102ac36a5f6fb8183f9fa27235c69a19349ffbcd
pgbarletta <pbarletta@gmail.com>
2023-12-05 12:07:23 -0300

    format

--- locuaz/aminoacidselector.py
+++ locuaz/aminoacidselector.py
@@ -41,15 +41,19 @@ class AminoAcidMemory:
         indices of the bins that are available for the next mutation for any
         epoch and any site. The BinPool will refill each time it's emptied.
     """
+
     # Branch names + site are the keys in these 2 dicts.
     # We use them to better decide the next mutation on each branch.
     branch_bins: Dict[Tuple[str, Site], Dict[int, Set[str]]]
     branch_bins_indices: Dict[Tuple[str, Site], BinPool]
     epoch_bins_indices: BinPool
 
-    def __init__(self, top_branches: Dict[str, Branch],
-                 bins: Dict[int, Set[str]],
-                 sites: List[Site]) -> None:
+    def __init__(
+        self,
+        top_branches: Dict[str, Branch],
+        bins: Dict[int, Set[str]],
+        sites: List[Site],
+    ) -> None:
         self.nbins = len(bins)
         self.epoch_bins_indices = BinPool.from_size(self.nbins)
 
@@ -58,32 +62,38 @@ class AminoAcidMemory:
         for site in sites:
             for branch_name in top_branches.keys():
                 self.branch_bins[(branch_name, site)] = deepcopy(bins)
-                self.branch_bins_indices[(branch_name, site)] = BinPool.from_size(self.nbins)
+                self.branch_bins_indices[(branch_name, site)] = BinPool.from_size(
+                    self.nbins
+                )
 
-    def remove_from_memory(self, branch_name: str, site: Site, bin_idx: int, aa: str) -> None:
+    def remove_from_memory(
+        self, branch_name: str, site: Site, bin_idx: int, aa: str
+    ) -> None:
         self.branch_bins_indices[(branch_name, site)].discard(bin_idx)
         self.epoch_bins_indices.discard(bin_idx)
         self.branch_bins[(branch_name, site)][bin_idx].discard(aa)
 
-    def get_bin(self, branch_name: str, site: Site,
-                old_aa: str, excluded_bins: Set[int]) -> int:
-
+    def get_bin(
+        self, branch_name: str, site: Site, old_aa: str, excluded_bins: Set[int]
+    ) -> int:
         while True:
             bin_idx = self.__attempt_novel_bin_idx__(branch_name, site, excluded_bins)
             bin_aas = self.branch_bins[(branch_name, site)][bin_idx]
+
             bin_aas.discard(old_aa)
             if len(bin_aas) > 0:
                 return bin_idx
             excluded_bins.add(bin_idx)
             if len(excluded_bins) == self.nbins:
-                raise GenerationError(f"Cannot mutate branch {branch_name}. "
-                                      "All amino acids from the available bins "
-                                      "have already been used.")
+                raise GenerationError(
+                    f"Cannot mutate branch {branch_name}. "
+                    "All amino acids from the available bins "
+                    "have already been used."
+                )
 
-    def __attempt_novel_bin_idx__(self,
-                                  branch_name: str,
-                                  site: Site,
-                                  excluded_bins: Set[int]) -> int:
+    def __attempt_novel_bin_idx__(
+        self, branch_name: str, site: Site, excluded_bins: Set[int]
+    ) -> int:
         """
         Tries to pick a random bin number that's available both in the branch
         and the epoch bin pools and isn't excluded.
@@ -135,6 +145,7 @@ class AminoAcidSelector:
     creation_config : Dict[str, Any]
         creation section from the input config file
     """
+
     use_bins: bool
     bins: Dict[int, Set[str]]
     aa_to_bin: Dict[str, Set[int]] = {"": set()}
@@ -161,22 +172,31 @@ class AminoAcidSelector:
             # pd_reis_barletta_full = pd.read_csv("reis_barletta_full.csv")
             # {r[1].AminoAcid: r[1].Probability for r in pd_reis_barletta_full.iterrows()}
 
-            pd_reis_barletta_cdr = pd.read_csv(Path(
-                Path(__file__).resolve().parent, "reis_barletta_cdr.csv"))
-            self.aa_distribution = {r[1].AminoAcid: r[1].Probability for r in pd_reis_barletta_cdr.iterrows()}
+            pd_reis_barletta_cdr = pd.read_csv(
+                Path(Path(__file__).resolve().parent, "reis_barletta_cdr.csv")
+            )
+            self.aa_distribution = {
+                r[1].AminoAcid: r[1].Probability
+                for r in pd_reis_barletta_cdr.iterrows()
+            }
 
         elif creation_config["aa_probability"] == "custom":
             self.aa_distribution = creation_config["custom"]
         else:
-            pd_uniform = pd.read_csv(Path(
-                Path(__file__).resolve().parent, "uniform.csv"))
-            self.aa_distribution = {r[1].AminoAcid: r[1].Probability for r in pd_uniform.iterrows()}
+            pd_uniform = pd.read_csv(
+                Path(Path(__file__).resolve().parent, "uniform.csv")
+            )
+            self.aa_distribution = {
+                r[1].AminoAcid: r[1].Probability for r in pd_uniform.iterrows()
+            }
 
-    def __call__(self,
-                 top_branches: Dict[str, Branch],
-                 branches: int,
-                 sites: List[Site],
-                 logger: Logger) -> Dict[str, List[Mutation]]:
+    def __call__(
+        self,
+        top_branches: Dict[str, Branch],
+        branches: int,
+        sites: List[Site],
+        logger: Logger,
+    ) -> Dict[str, List[Mutation]]:
         """
         Generate mutations according to user input (bins, probability of
         choosing each amino acid, etc...).
@@ -206,8 +226,8 @@ class AminoAcidSelector:
                 try:
                     new_aa = self.__select_aa__(branch.branch_name, site, old_aa)
                     # Add this mutation to this branch.
-                    mutations[branch.branch_name].append(Mutation.from_site(
-                        site, old_aa=old_aa, new_aa=new_aa)
+                    mutations[branch.branch_name].append(
+                        Mutation.from_site(site, old_aa=old_aa, new_aa=new_aa)
                     )
                 except GenerationError as e:
                     logger.warning(f"{e}")
@@ -216,32 +236,36 @@ class AminoAcidSelector:
         return mutations
 
     def __select_aa__(self, branch_name: str, site: Site, old_aa: str) -> str:
-
         print(self.aa_distribution)
 
         if self.bins_criteria == "without":
             excluded_bins: Set[int] = set(self.aa_to_bin[old_aa])
         else:
             # 'within'. All bins but the one that corresponds to the aa are excluded.
-            excluded_bins: Set[int] = set(range(0, len(self.bins))) - self.aa_to_bin[old_aa]
+            excluded_bins: Set[int] = (
+                set(range(0, len(self.bins))) - self.aa_to_bin[old_aa]
+            )
 
         bin_idx = self.memory.get_bin(branch_name, site, old_aa, excluded_bins)
         # Normalize the probabilities of the AAs in the bin, so they add up to 1
-        bin_aas = self.__get_norm_bin__(self.aa_distribution,
-                                        self.memory.branch_bins[(branch_name, site)][bin_idx])
+        bin_aas = self.__get_norm_bin__(
+            self.aa_distribution, self.memory.branch_bins[(branch_name, site)][bin_idx]
+        )
         new_aa = choices(list(bin_aas.keys()), list(bin_aas.values()))[0]
         self.memory.remove_from_memory(branch_name, site, bin_idx, new_aa)
 
         return new_aa
 
-    def __get_norm_bin__(self,
-                         distro: Dict[Union[str, int], float],
-                         aa_bin: Set[str]) -> Dict[Union[str, int], float]:
+    def __get_norm_bin__(
+        self, distro: Dict[Union[str, int], float], aa_bin: Set[str]
+    ) -> Dict[Union[str, int], float]:
         return self.__norm_distribution__(
-            {aa: prob for aa, prob in distro.items() if aa in aa_bin})
+            {aa: prob for aa, prob in distro.items() if aa in aa_bin}
+        )
 
     @staticmethod
-    def __norm_distribution__(distro: Dict[Union[str, int], float]
-                              ) -> Dict[Union[str, int], float]:
+    def __norm_distribution__(
+        distro: Dict[Union[str, int], float]
+    ) -> Dict[Union[str, int], float]:
         tot = sum(distro.values())
         return {aa: prob / tot for aa, prob in distro.items()}
